37ff03a629c0aecfb1e606db1fdf6f0f3c2d9841
eldiyar312 <eldiyar.san@gmail.com>
2020-08-24 02:42:29 +0600

    Updates

---
+++ precache-manifest.69fe2512b50387254d944fa944cccdea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07dec581e32a2f78e3d813e425fae5f6",
+    "url": "/record-patient/index.html"
+  },
+  {
+    "revision": "d251dd6f174d58fbea8b",
+    "url": "/record-patient/static/js/2.4c05f77a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/record-patient/static/js/2.4c05f77a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44454e4337f34ca469b0",
+    "url": "/record-patient/static/js/main.a059f17a.chunk.js"
+  },
+  {
+    "revision": "a018f13d77cf05b4dac2",
+    "url": "/record-patient/static/js/runtime-main.20fea772.js"
+  }
+]);
